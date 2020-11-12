79e06a3fdb06daa008dc00dae260f2032a0feccf
J <Jordan.Roman90@gmail.com>
2020-11-11 20:32:11 -0500

    added bootstrap

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,12 @@
-import { Component } from "React";
+import { Component } from "react";
+class Header extends Component {
 
+    render() {
+        return (
+            <h1>Jordan Roman</h1>
+        )
+    }
+}
 
 
 export default Header;
