53f363ff2309a8e058cc9b404bca8ddc0e2e2f8c
J <Jordan.Roman90@gmail.com>
2020-11-30 21:16:58 -0500

    styling of projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
 import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
 import Header from './Header'
-
+import './Project.css'
 
 class Projects extends Component {
     render() {
@@ -10,16 +10,16 @@ class Projects extends Component {
                 <h1>Stuff I've been working on</h1>
 
                 <div class="work-container pt-2 pb-2" style={{borderTop: '1px solid black', borderBottom: '1px solid black'}}>
-                    <h3 class="title"><Link to="/projects/mshortstories">mShortStories</Link> </h3>
+                    <h3 class="title pt-1 pb-1 mb-0"><Link class="abc" to="/projects/mshortstories">mShortStories</Link> </h3>
                 </div>
 
                 <div class="work-container pt-2 pb-2" style={{borderBottom: '1px solid black'}}>
-                    <h3 class="title"><Link to="/projects/mshortstories">Chip8 Emulator</Link> </h3>
+                    <h3 class="title pt-1 pb-1 mb-0"><Link to="/projects/chip8">Chip8 Emulator</Link> </h3>
                 </div>
 
-                <div class="work-container pt-2 pb-2" style={{borderBottom: '1px solid black'}}>
-                    <h3 class="title"><Link to="/projects/mshortstories">Just Good Games</Link> </h3>
-                </div>
+                {/* <div class="work-container pt-2 pb-2" style={{borderBottom: '1px solid black'}}>
+                    <h3 class="title pt-1 pb-1 mb-0"><Link to="/projects/jgg">Just Good Games</Link> </h3>
+                </div> */}
 
             </div>
         )
